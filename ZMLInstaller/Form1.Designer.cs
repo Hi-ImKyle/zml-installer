3dff9beeacc8e6a37fcde5f720be50d87c4d0f7d
Kyle <kyle.fisher17552@gmail.com>
2024-01-01 20:29:15 +0000

    Init

---
+++ ZMLInstaller/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ZMLInstaller
+{
+    partial class InstallerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstallerForm));
+            this.OpenDirButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Step2DownloadProgress = new System.Windows.Forms.ProgressBar();
+            this.Step2DownloadButton = new System.Windows.Forms.Button();
+            this.Step2Label = new System.Windows.Forms.Label();
+            this.Step3Label = new System.Windows.Forms.Label();
+            this.Step3InstallButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenDirButton
+            // 
+            this.OpenDirButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(43)))));
+            this.OpenDirButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(25)))), ((int)(((byte)(29)))));
+            this.OpenDirButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenDirButton.Location = new System.Drawing.Point(51, 8);
+            this.OpenDirButton.Name = "OpenDirButton";
+            this.OpenDirButton.Size = new System.Drawing.Size(249, 23);
+            this.OpenDirButton.TabIndex = 0;
+            this.OpenDirButton.Text = "Select your CarX Folder";
+            this.OpenDirButton.UseVisualStyleBackColor = false;
+            this.OpenDirButton.Click += new System.EventHandler(this.OpenDirButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Step 1";
+            // 
+            // Step2DownloadProgress
+            // 
+            this.Step2DownloadProgress.Enabled = false;
+            this.Step2DownloadProgress.Location = new System.Drawing.Point(127, 37);
+            this.Step2DownloadProgress.Name = "Step2DownloadProgress";
+            this.Step2DownloadProgress.Size = new System.Drawing.Size(172, 23);
+            this.Step2DownloadProgress.TabIndex = 2;
+            // 
+            // Step2DownloadButton
+            // 
+            this.Step2DownloadButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(43)))));
+            this.Step2DownloadButton.Enabled = false;
+            this.Step2DownloadButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(25)))), ((int)(((byte)(29)))));
+            this.Step2DownloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Step2DownloadButton.Location = new System.Drawing.Point(51, 37);
+            this.Step2DownloadButton.Name = "Step2DownloadButton";
+            this.Step2DownloadButton.Size = new System.Drawing.Size(70, 23);
+            this.Step2DownloadButton.TabIndex = 3;
+            this.Step2DownloadButton.Text = "Download";
+            this.Step2DownloadButton.UseVisualStyleBackColor = false;
+            this.Step2DownloadButton.Click += new System.EventHandler(this.Step2DownloadButton_Button);
+            // 
+            // Step2Label
+            // 
+            this.Step2Label.AutoSize = true;
+            this.Step2Label.Enabled = false;
+            this.Step2Label.Location = new System.Drawing.Point(7, 42);
+            this.Step2Label.Name = "Step2Label";
+            this.Step2Label.Size = new System.Drawing.Size(38, 13);
+            this.Step2Label.TabIndex = 4;
+            this.Step2Label.Text = "Step 2";
+            // 
+            // Step3Label
+            // 
+            this.Step3Label.AutoSize = true;
+            this.Step3Label.Enabled = false;
+            this.Step3Label.Location = new System.Drawing.Point(7, 71);
+            this.Step3Label.Name = "Step3Label";
+            this.Step3Label.Size = new System.Drawing.Size(38, 13);
+            this.Step3Label.TabIndex = 5;
+            this.Step3Label.Text = "Step 3";
+            // 
+            // Step3InstallButton
+            // 
+            this.Step3InstallButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(43)))));
+            this.Step3InstallButton.Enabled = false;
+            this.Step3InstallButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(25)))), ((int)(((byte)(29)))));
+            this.Step3InstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Step3InstallButton.Location = new System.Drawing.Point(51, 66);
+            this.Step3InstallButton.Name = "Step3InstallButton";
+            this.Step3InstallButton.Size = new System.Drawing.Size(249, 23);
+            this.Step3InstallButton.TabIndex = 6;
+            this.Step3InstallButton.Text = "Install";
+            this.Step3InstallButton.UseVisualStyleBackColor = false;
+            this.Step3InstallButton.Click += new System.EventHandler(this.Step3InstallButton_Click);
+            // 
+            // InstallerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(434, 235);
+            this.Controls.Add(this.Step3InstallButton);
+            this.Controls.Add(this.Step3Label);
+            this.Controls.Add(this.Step2Label);
+            this.Controls.Add(this.Step2DownloadButton);
+            this.Controls.Add(this.Step2DownloadProgress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpenDirButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InstallerForm";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZML Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenDirButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar Step2DownloadProgress;
+        private System.Windows.Forms.Button Step2DownloadButton;
+        private System.Windows.Forms.Label Step2Label;
+        private System.Windows.Forms.Label Step3Label;
+        private System.Windows.Forms.Button Step3InstallButton;
+    }
+}
+
---
+++ ZMLInstaller/Form1.cs
@@ -0,0 +1,119 @@
+﻿using Ookii.Dialogs.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZMLInstaller
+{
+    public partial class InstallerForm : Form
+    {
+        public InstallerForm()
+        {
+            InitializeComponent();
+        }
+
+        private string GameDir;
+        private byte[] ZMLBundleData;
+
+        private void OpenDirButton_Click(object sender, EventArgs e)
+        {
+            var folderBrowser = new VistaFolderBrowserDialog();
+            folderBrowser.Description = "Select your CarX Folder";
+            folderBrowser.UseDescriptionForTitle = true;
+
+            var result = folderBrowser.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                CheckSelectedPath(folderBrowser.SelectedPath);
+            }
+        }
+
+        private void CheckSelectedPath(string path)
+        {
+            var exePath = Path.Combine(path, "Drift Racing Online.exe");
+            var gameAssemblyPath = Path.Combine(path, "GameAssembly.dll");
+            if (File.Exists(exePath))
+            {
+                if (File.Exists(gameAssemblyPath))
+                {
+                    var taskDialog = new TaskDialog();
+                    taskDialog.WindowTitle = "ZML Installer";
+                    taskDialog.MainIcon = TaskDialogIcon.Warning;
+                    taskDialog.MainInstruction = "You appear to be on the LIVE version of the game";
+                    taskDialog.Content = "You need to be on the Moddable version. Here are the steps.\n\nOpen Steam.\nRight Click on CarX in your Library\nProperties\nBetas\nBeta participation, select \"moddable\"\n\nYou will need to wait for the game to update to the moddable version to try installing ZML again.";
+                    taskDialog.Buttons.Add(new TaskDialogButton("Retry"));
+                    taskDialog.Buttons.Add(new TaskDialogButton(ButtonType.Cancel));
+                    var result = taskDialog.ShowDialog();
+                    if (result.Text == "Retry")
+                    {
+                       CheckSelectedPath(path);
+                    }
+                    return;
+                }
+                GameDir = path;
+                EnableStep2();
+            }
+            else
+            {
+                MessageBox.Show("Invalid CarX Folder", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void Step2DownloadButton_Button(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            var webClient = new System.Net.WebClient();
+            webClient.DownloadProgressChanged += (s, pe) =>
+            {
+                Step2DownloadProgress.Value = pe.ProgressPercentage;
+            };
+            ZMLBundleData = await webClient.DownloadDataTaskAsync("https://cdn.discordapp.com/attachments/938577547514499152/1174746809831276684/ZML_Bundle_11-16.zip?ex=6596dbb9&is=658466b9&hm=49137a7696e649618db51b8a9bb7f54ec7955f0b50245a42a0afa68d15e4df76&");
+            this.Enabled = true;
+            EnableStep3();
+        }
+
+        public void EnableStep2()
+        {
+            Step2Label.Enabled = true;
+            Step2DownloadButton.Enabled = true;
+            Step2DownloadProgress.Value = 0;
+            Step2DownloadProgress.Enabled = true;
+        }
+
+        public void EnableStep3()
+        {
+            Step3Label.Enabled = true;
+            Step3InstallButton.Enabled = true;
+        }
+
+        private void Step3InstallButton_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            var zipFile = new ZipArchive(new MemoryStream(ZMLBundleData), ZipArchiveMode.Read);
+            var firstFolder = zipFile.Entries.First().FullName;
+            var entries = zipFile.Entries.Where(x => !string.IsNullOrWhiteSpace(x.FullName.Replace(firstFolder, ""))).ToList();
+            entries.ForEach(x =>
+            {
+                var path = Path.Combine(GameDir, x.FullName.Replace(firstFolder, ""));
+                if (x.FullName.EndsWith("/"))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else
+                {
+                    x.ExtractToFile(path, true);
+                }
+            });
+            MessageBox.Show("ZML Installed!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Enabled = true;
+        }
+    }
+}
